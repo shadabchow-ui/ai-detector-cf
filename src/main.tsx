5330ab254eee1659ed191898263c44dfbc06a13b
shadabchow-ui <shadabchow@gmail.com>
2026-01-21 11:36:01 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css' // ✅ must be exactly this path
+import './styles.css' // REQUIRED
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
 
